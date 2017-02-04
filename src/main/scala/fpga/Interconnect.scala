f7f7ea84109560c8ca7d4d5ce28f45556de265b3
Alex Guo <xfguo@xfguo.org>
2017-02-04 17:43:46 +0800

    interconnect: connect carry in/out to in/out. stall no unittest.
    Signed-off-by: Alex Guo <xfguo@xfguo.org>

--- src/main/scala/fpga/Interconnect.scala
+++ src/main/scala/fpga/Interconnect.scala
@@ -25,21 +25,36 @@ class InterconnectIo extends Bundle {
 class Interconnect extends Module {
   val lutx = 4 /* input number of LUT */
   val le_num = 8 /* number of logic elements in PLB */
+  var carry_in_num = 1 /* carry out from PLB */
+  var carry_out_num = 1 /* carry out from PLB */
   
-  val in_sel_width = UInt(2 * 4 - 1).getWidth /* bits of number mux select number */
+  val in_sel_width = UInt(2 * 4 - 1 + carry_in_num).getWidth /* bits of number mux select number (with carry in) */
 
   val out_num = 4 /* output wires number */
-  val out_sel_width = UInt(le_num - 1).getWidth /* bits of output mux select number */
+  val out_sel_width = UInt(le_num - 1 + carry_out_num).getWidth /* bits of output mux select number (with carry out) */
 
   val io = IO(new InterconnectIo)
 
-  val out_sel_sreg = Reg(UInt((out_num * out_sel_width).W))
-  val in_sel_sreg = Reg(UInt((le_num * lutx * in_sel_width).W))
+  val out_sel_sreg = Reg(UInt((out_num * out_sel_width).W)) /* lut out & carry out sels */
+  val in_sel_sreg = Reg(UInt((le_num * lutx * in_sel_width).W)) /* lut in & carry in sels */
 
-  /* TODO: carry sel */
+  /* input mux: io port => PLB in (lut and carry) */
+  val in_mux = Module(new MuxMtoN(2 * 4, le_num * lutx + carry_in_num))
 
-  /* output mux */
-  val out_mux = Module(new MuxMtoN(le_num, out_num))
+  in_mux.io.ins := io.ins
+
+  for (i <- 0 to le_num - 1) {
+    io.plbio.logics(i).in := Cat( (0 to lutx - 1).map(x => in_mux.io.outs( x + i * lutx ) ) )
+  }
+
+  io.plbio.carry.in := in_mux.io.outs(le_num * lutx)
+  
+  for (i <- 0 to le_num * lutx - 1) {
+    in_mux.io.sels(i) := in_sel_sreg((i + 1) * in_sel_width - 1, i * in_sel_width)
+  }
+  
+  /* output mux: PLB output (lut and carry) => io port */
+  val out_mux = Module(new MuxMtoN(le_num + carry_out_num, out_num))
 
   io.outs := out_mux.io.outs
 
@@ -47,21 +62,10 @@ class Interconnect extends Module {
     out_mux.io.ins(i) := io.plbio.logics(i).out
   }
 
+  out_mux.io.ins(le_num) := io.plbio.carry.out
+
   for (i <- 0 to out_num - 1) {
     out_mux.io.sels(i) := out_sel_sreg((i + 1) * out_sel_width - 1, i * out_sel_width)
-  }
-
-  /* input mux */
-  val in_mux = Module(new MuxMtoN(2 * 4, le_num * lutx))
-
-  in_mux.io.ins := io.ins
-
-  for (i <- 0 to le_num - 1) {
-    io.plbio.logics(i).in := Cat( (0 to lutx - 1).map(x => in_mux.io.outs( x + i * lutx ) ) )
-  }
-  
-  for (i <- 0 to le_num * lutx - 1) {
-    in_mux.io.sels(i) := in_sel_sreg((i + 1) * in_sel_width - 1, i * in_sel_width)
   }
 
   /* config */
